1933b655b46242c558cec03cc8768d231ec4eed8
mryhryki <mryhryki@gmail.com>
2022-01-16 14:05:29 +0900

    Update .eslintrc.yaml

--- packages/lint/src/module.js
+++ packages/lint/src/module.js
@@ -3,33 +3,6 @@ const { ESLint } = require("eslint");
 const { existFileOrDirectory, getFile, writeFile } = require("./util");
 
 /**
- * @link https://eslint.org/docs/user-guide/configuring/
- */
-const baseConfig = {
-  extends: ["plugin:@typescript-eslint/recommended", "prettier"],
-  plugins: ["jest", "@typescript-eslint", "prettier"],
-  env: {
-    commonjs: true,
-    es2020: true,
-    "jest/globals": true,
-    node: true,
-  },
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": "error",
-    "@typescript-eslint/no-var-requires": "off",
-  },
-};
-
-/**
  * @link https://eslint.org/docs/developer-guide/nodejs-api
  * @param fix boolean
  * @return {Promise<void>}
@@ -42,9 +15,7 @@ const lint = async (fix) => {
     const editorConfig = `${currentDir}/.editorconfig`;
 
     if (!(await existFileOrDirectory(eslintrc))) {
-      const packageJson = require(`${currentDir}/package.json`);
-      const useReact = packageJson?.dependencies?.react != null || packageJson?.devDependencies?.react != null;
-      const config = await getFile(`.eslintrc.${useReact ? "react" : "node"}.yaml`);
+      const config = await getFile(".eslintrc.yaml");
       await writeFile(eslintrc, config);
     }
 
@@ -58,7 +29,7 @@ const lint = async (fix) => {
       await writeFile(editorConfig, config);
     }
 
-    const eslint = new ESLint({ baseConfig, fix, errorOnUnmatchedPattern: false });
+    const eslint = new ESLint({ fix, errorOnUnmatchedPattern: false });
     const patterns = [
       `${currentDir}/*.js`,
       `${currentDir}/*.jsx`,
